255bff0d38c669f2c66cd421b42e8227ef1f47b9
aleksei24 <lexa24032@gmail.com>
2020-10-28 20:19:25 +0300

    variables have been made

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const clear = document.querySelector('.clear');
+const date = document.querySelector('#date');
+const list = document.querySelector('#list');
+const input = document.querySelector('#input');
+
+const CHECK = 'done';
+const UNCHECK = 'co';
+const binAttr = '../img/bin.png';
+const checkedAttr = '../img/done.png';
+const uncheckedAttr = '../img/task.png';
+const crossed = 'lineThrough';
+
+let arrList;
+let id;

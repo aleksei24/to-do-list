11030cce416dd152548b3d73757579d8f474b593
aleksei24 <lexa24032@gmail.com>
2020-10-28 20:22:15 +0300

    a function that add a task to a list has been made

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ const crossed = 'lineThrough';
 
 let arrList;
 let id;
+
+function addTask(task, id, done, bin) {
+    if (bin) {
+        return;
+    }
+    const DONE = done ? CHECK : UNCHECK;
+    const ATTRSRC = done ? checkedAttr : uncheckedAttr;
+    const LINE = done ? crossed : '';
+    const text = `<li class="item">
+                    <img class="${DONE}" src="${ATTRSRC}" 
+                    alt="" job="complete" id="${id}" />
+                    <p class="text ${LINE}">${task}</p>
+                    <img class="de" src="${binAttr}" alt="bin"
+                    job="delete" id="${id}" />
+                </li>`;
+    const position = 'beforeend';
+    list.insertAdjacentHTML(position, text);
+}
